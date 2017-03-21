18ae6bca31809f1a04c4112dffe81b18eb5f0927
Strawberry <strawberrycsgo@gmail.com>
2017-03-21 09:31:53 +0700

    login admin 2

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,42 @@
 class AdminsController < ApplicationController
   before_action :logged_in_admin
-  before_action :find_admin
 
   def show
-    unless @admin
+    unless @current_admin
       flash[:danger] = t "admin.error_log"
       redirect_to root_url
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @current_admin.authenticate params[:admin][:old_password]
+      if @current_admin.update_attributes select_params
+        flash[:success] = t "admin.success"
+        redirect_to admin_path
+      else
+        flash[:danger] = t "admin.fail"
+        render :edit
+      end
+    else
+      flash[:danger] = t "admin.fail"
+      render :edit
+    end
+  end
+
+  private
+  def admin_params
+    params.require(:admin).permit :name, :email, :old_password, :password,
+      :password_confirmation
+  end
+
+  def select_params
+    if admin_params[:password]== ""
+      params.require(:admin).permit :name, :email
+    elsif admin_params[:password] == admin_params[:password_confirmation]
+      params.require(:admin).permit :name, :email, :password
     end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     admin = Admin.find_by email: params[:session][:email].downcase
     if admin && admin.authenticate(params[:session][:password])
       log_in admin
-      redirect_to root_url
+      redirect_to admin
     else
       flash.now[:danger] = t "admin.error_wrongpass"
       render :new
