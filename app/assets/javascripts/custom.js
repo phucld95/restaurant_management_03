e6baaf4053af6848aad6bfbf21d22ed781873155
Ohmygodvt95 <ohmygodvt95@gmail.com>
2017-07-04 15:05:49 +0700

    Book table step by step

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,51 +1,4 @@
 /*global WOW:true*/
-var wow = new WOW(
-  {
-    boxClass: 'wow',
-    animateClass: 'animated',
-    offset: 0,
-    mobile: true,
-    live: true,
-    scrollContainer: null
-  }
-);
-wow.init();
-
-$(document).on('click', '.edit-pass', function() {
-  $('#edit_pass_show').toggleClass('edit-pass-content-hide', 'edit-pass-content-show');
-});
-$(document).on('click', '#searchclear', function(){
-  $('#searchinput').val('');
-});
-
-$(document).on('click', '#btn-find-table', function() {
-  var val_cap = $('#capacity_field').val();
-  var val_date = $('#date_field').val();
-  var val_time = $('#time_field').val();
-  var val_id = $('#chosen_table_value').val();
-  $('#btn-summit-table').css('display','inline-block');
-  $.ajax({
-    type:'GET',
-    url: '/tables',
-    dataType: 'json',
-    data: {
-      table: {
-        capacity_gteq: val_cap,
-        orders_day_eq: val_date,
-        orders_time_in_eq: val_time,
-        id: val_id
-      }
-    }
-  }).done(  function(data) {
-    for(var i=0;i<19;i++) {
-      $('#btn-'+ i).removeClass('btn-maru').removeClass('btn-choose').addClass('btn-batsu');
-    }
-    $.each(data, function (index, element) {
-      $('#btn-' + element.code).addClass('btn-maru').removeClass('btn-batsu');
-    });
-  });
-});
-
 $(document).ready(function(){
   $('.datepicker2').datepicker({
     dateFormat: 'yy-mm-dd',
@@ -54,69 +7,54 @@ $(document).ready(function(){
     gotoCurrent: true,
     minDate: 0
   });
-});
 
-$(document).ready(function(){
   $('.btn-table').click(function(){
     $('.btn-table').removeClass('btn-choose');
     $(this).addClass('btn-choose');
   });
-});
 
-$(document).on('click', '#btn-summit-table', function(e) {
-  e.preventDefault();
-  var a = 0;
-  var val_cap = $('#capacity_field').val();
-  var val_date = $('#date_field').val();
-  var val_time = $('#time_field').val();
-  if(val_cap == '' || val_date == '' || val_time == '') {
-    a = 1;
-    $('#popup1').removeClass('hide');
-    $('.alert1').removeClass('hide');
-    $('.alert2').addClass('hide');
-  } else if (!$('.btn-table').hasClass('btn-choose')) {
-    a = 2;
-  }
-  if(a == 0) {
-    $('#guest-popup').css('display','block');
-  } else if(a == 2) {
-    $('#popup1').removeClass('hide');
-    $('.alert2').removeClass('hide');
-    $('.alert1').addClass('hide');
-  }
-});
-
-$(document).on('click','.close', function(){
-  $('#myModal').css('display','none');
-  $('#guest-popup').css('display','none');
-});
-
-$(document).on('click','#btn-submit-guest', function() {
-  $(this).hide();
-  $('#btn-submit-order').show();
-  $('#guest-info').hide();
-  $('#hide-after-guest-info').hide();
-  var val_name = $('#name-guest').val();
-  var val_phone = $('#phone-guest').val();
-  var val_email = $('#email-guest').val();
-  $.ajax({
-    type:'POST',
-    url: '/guests',
-    dataType: 'json',
-    data: {
-      guest: {
-        name: val_name,
-        email: val_email,
-        phone_num: val_phone,
-      }
-    }
-  }).success(function(){
-    book_table();
+  $(document).on('click','.close', function(){
+    $('#myModal').css('display','none');
+    $('#guest-popup').css('display','none');
   });
-});
 
-$(document).on('click','#register-guest-code', function() {
-  $('#myModal').css('display','block');
+  var wow = new WOW(
+    {
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 0,
+      mobile: true,
+      live: true,
+      scrollContainer: null
+    }
+  );
+  wow.init();
+
+  $(document).on('click', '.edit-pass', function() {
+    $('#edit_pass_show').toggleClass('edit-pass-content-hide', 'edit-pass-content-show');
+  });
+
+  $(document).on('click', '#searchclear', function(){
+    $('#searchinput').val('');
+  });
+
+  $('.home-overlay').bgswitcher({
+    images: [
+      'http://i.imgur.com/WVDh6N2.jpg',
+      'http://i.imgur.com/gQGFY9C.jpg',
+      'http://i.imgur.com/ITUizuk.jpg',
+      'http://i.imgur.com/4MzLYrL.jpg',
+      'http://i.imgur.com/g8AiwIu.jpg',
+      'http://i.imgur.com/oryEB6Z.jpg',
+      'http://i.imgur.com/YRk2lI7.jpg',
+      'http://i.imgur.com/7uO7iFA.jpg'],
+    effect: 'fade',
+    interval: 5000,
+    loop: true,
+    shuffle: true,
+    duration: 500,
+    easing: 'linear'
+  });
 });
 
 function book_table (){
@@ -139,43 +77,174 @@ function book_table (){
   });
 }
 
-$(document).on('click', '#btn-submit-guest-code',function(){
-  var guest_code = $('#code-guest').val();
-  $.ajax({
-    type:'GET',
-    url: '/guests',
-    dataType: 'json',
-    data: {
-      guest: {
-        code: guest_code,
-      }
-    }
-  }).success(function(){
-    book_table();
-  });
-});
-
 $(document).on('click','#close-alert', function(){
   $('#popup1').addClass('hide');
 });
 
 $(document).ready(function(){
-  $('.home-overlay').bgswitcher({
-    images: [
-      'http://i.imgur.com/WVDh6N2.jpg',
-      'http://i.imgur.com/gQGFY9C.jpg',
-      'http://i.imgur.com/ITUizuk.jpg',
-      'http://i.imgur.com/4MzLYrL.jpg',
-      'http://i.imgur.com/g8AiwIu.jpg',
-      'http://i.imgur.com/oryEB6Z.jpg',
-      'http://i.imgur.com/YRk2lI7.jpg',
-      'http://i.imgur.com/7uO7iFA.jpg'],
-    effect: 'fade',
-    interval: 5000,
-    loop: true,
-    shuffle: true,
-    duration: 500,
-    easing: 'linear'
+  var current_fs, next_fs, previous_fs;
+  var current, next, prev;
+  var left, opacity, scale;
+  var animating;
+
+  function next_step(element) {
+    if(animating) return false;
+    animating = true;
+    current = $(element).attr('data-current');
+    next = $(element).attr('data-next');
+
+    current_fs = $('#block-' + current);
+    next_fs = $('#block-' + next);
+
+    $('#progressbar li:nth(' + next + ')').addClass('active');
+
+    current_fs.animate({opacity: 0}, {
+      step: function(now) {
+        scale = 1 - (1 - now) * 0.2;
+        left = (now * 50)+'%';
+        opacity = 1 - now;
+        current_fs.css({
+          'transform': 'scale('+scale+')'
+        });
+        next_fs.css({'left': left, 'opacity': opacity});
+      },
+      duration: 500,
+      complete: function(){
+        current_fs.hide();
+        animating = false;
+        next_fs.show();
+      },
+      easing: 'easeInOutBack'
+    });
+  }
+
+  function prev_step(element) {
+
+    if(animating) return false;
+    animating = true;
+
+    current = $(element).attr('data-current');
+    prev = $(element).attr('data-prev');
+
+    current_fs = $('#block-' + current);
+    previous_fs = $('#block-' + prev);
+
+    $('#progressbar li:nth(' + current + ')').removeClass('active');
+
+    previous_fs.show();
+
+    current_fs.animate({opacity: 0}, {
+      step: function(now) {
+        scale = 0.8 + (1 - now) * 0.2;
+        left = ((1-now) * 50) + '%';
+        opacity = 1 - now;
+        current_fs.css({'left': left});
+        previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+        current_fs.hide();
+      },
+      duration: 400,
+      complete: function(){
+        current_fs.hide();
+        animating = false;
+      },
+      easing: 'easeInOutBack'
+    });
+  }
+
+  $('.next').click(function(){
+    var element = this;
+    var step = $(this).attr('step');
+    if(step == 1){
+      var val_cap = $('#capacity_field').val();
+      var val_date = $('#date_field').val();
+      var val_time = $('#time_field').val();
+      var val_id = $('#chosen_table_value').val();
+
+      if(val_cap == '' || val_date == '' || val_time == ''){
+        $('.alert-1').addClass('show');
+        setTimeout(function () {
+          $('.alert-1').removeClass('show');
+        }, 2500);
+        return;
+      }
+
+      $('#btn-summit-table').css('display','inline-block');
+      $.ajax({
+        type:'GET',
+        url: '/tables',
+        dataType: 'json',
+        data: {
+          table: {
+            capacity_gteq: val_cap,
+            orders_day_eq: val_date,
+            orders_time_in_eq: val_time,
+            id: val_id
+          }
+        }
+      }).done(function(data) {
+        for(var i=0;i<19;i++) {
+          $('#btn-'+ i).removeClass('btn-maru').removeClass('btn-choose').addClass('btn-batsu');
+        }
+        $.each(data, function (index, element) {
+          $('#btn-' + element.code).addClass('btn-maru').removeClass('btn-batsu');
+        });
+        next_step(element);
+      });
+    }
+    else if(step == 2){
+      if($('.btn-table').hasClass('btn-choose')){
+        next_step(element);
+      }
+      else{
+        $('.alert-2').addClass('show');
+        setTimeout(function () {
+          $('.alert-2').removeClass('show');
+        }, 2500);
+        return;
+      }
+    }
+    else if(step == 4){
+      var guest_code = $('#code-guest').val();
+      $.ajax({
+        type:'GET',
+        url: '/guests',
+        dataType: 'json',
+        data: {
+          guest: {
+            code: guest_code,
+          }
+        }
+      }).success(function(){
+        book_table();
+      });
+    }
+    else if(step == 5){
+      var val_name = $('#name-guest').val();
+      var val_phone = $('#phone-guest').val();
+      var val_email = $('#email-guest').val();
+      $.ajax({
+        type:'POST',
+        url: '/guests',
+        dataType: 'json',
+        data: {
+          guest: {
+            name: val_name,
+            email: val_email,
+            phone_num: val_phone,
+          }
+        }
+      }).success(function(){
+        book_table();
+      });
+    }
+  });
+
+  $('.previous').click(function(){
+    prev_step(this);
+  });
+
+  $('.submit').click(function(){
+    return false;
   });
 });
 
